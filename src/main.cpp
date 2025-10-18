5c36c9163aab0bd0c49c2b50882b56d8466b01c6
robin l'hyver <robinlhyver@gmail.com>
2025-10-18 11:02:01 +0200

    Creation of Webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,86 @@
 #include <BME.hpp>
 #include <heltec_unofficial.h>
 #include <TMG3993.hpp>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
+#include "LittleFS.h"
+#include <ArduinoJson.h>
+#include <String.h>
 
-
-// put fuCnction declarations here:
 char buf[30];
+
+const char* ssid = "TelRobin";
+const char* password = "robinestbeau";
+
+
+WebServer server(80);
+
+void handleApiData() {
+    DynamicJsonDocument doc(4096);
+    // Données intérieures
+    doc["temperature"] = bme.temperature;
+    doc["humidity"] = bme.humidity;
+    doc["pressure"] = bme.pressure;
+    // Données extérieures
+    doc["temp_today"] = 0;
+    doc["humidity_today"] = 0;
+    doc["weather_today"] = 0;
+    doc["id_weather_today"] = 0;
+    doc["lastupdate"] = 0;
+    doc["ip"] = WiFi.localIP().toString();
+    // Pluie à venir
+    JsonArray rain = doc.createNestedArray("rainfallhour");
+    for (int i = 0; i < 12; i++) rain.add(0);
+    // Prévisions 6 jours
+    JsonArray forecast = doc.createNestedArray("forecast");
+    for (int i = 0; i < 6; i++) {
+        JsonObject day = forecast.createNestedObject();
+        day["day"] = ""; // tu peux ajouter le nom du jour si tu veux
+        day["temp_min"] = 0;
+        day["temp_max"] = 0;
+        day["humidity"] = 0;
+        day["id_weather"] = 0;
+    }
+    String json;
+    serializeJson(doc, json);
+    server.send(200, "application/json", json);
+}
+
+void handleIndex() {
+    File file = LittleFS.open("/index.html","r");
+    if (!file) {
+        server.send(404, "text/plain", "Fichier index.html non trouvé");
+        return;
+    }
+    server.streamFile(file, "text/html");
+    file.close();
+}
 
 
 void setup() {
   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  if (!LittleFS.begin()) {
+    Serial.println("Erreur LittleFS");
+    while (1);
+  }
+
+  server.on("/", handleIndex);
+  server.on("/api/data", handleApiData);
+
+  // Start the server
+  server.begin();
+
   heltec_setup();
 
   display.setFont(ArialMT_Plain_10);
@@ -20,12 +92,13 @@ void setup() {
 }
 
 void loop() {
+  server.handleClient();
+  display.clear();
+
   getDataTMG3993();
   Serial.println();
-  Serial.print("Looping...");
   getDataBME();
   display.drawString(0,0,"Hello, world!");
-  delay(2000);
 
   snprintf(buf, sizeof(buf), "T: %.2f C", bme.temperature);
   display.drawString(10,10,buf);
